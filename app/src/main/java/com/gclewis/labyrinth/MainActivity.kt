17109d1f0b984549292d9f94f5435dadda451f58
Graham Charles Lewis <grahamcharleslewis@gmail.com>
2024-08-03 11:49:18 +0100

    WiP

--- app/src/main/java/com/gclewis/labyrinth/MainActivity.kt
+++ app/src/main/java/com/gclewis/labyrinth/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gclewis.labyrinth.ui.theme.LabyrinthTheme
+import com.gclewis.labyrinth.ui.theme.LabyrinthTypography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +37,29 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+    Column(
         modifier = modifier
-    )
+            .padding(16.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier
+                .padding(16.dp)
+                .fillMaxSize(),
+            style = LabyrinthTypography.displayLarge
+        )
+        Text(
+            text = "An amazeing app",
+            modifier = modifier
+                    .padding(16.dp)
+                    .fillMaxSize(),
+            style = LabyrinthTypography.bodyMedium
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
