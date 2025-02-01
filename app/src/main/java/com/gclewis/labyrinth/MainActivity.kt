78d162083bd2d12b72c0d4c19dbb92907398c394
Graham Charles Lewis <grahamcharleslewis@gmail.com>
2025-02-01 10:18:22 +0000

    Update welcome page

--- app/src/main/java/com/gclewis/labyrinth/MainActivity.kt
+++ app/src/main/java/com/gclewis/labyrinth/MainActivity.kt
@@ -6,13 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.gclewis.labyrinth.ui.theme.LabyrinthTheme
@@ -25,8 +32,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LabyrinthTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Labyrinth",
+                    Welcome(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -36,29 +42,64 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Welcome(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
-            .padding(16.dp)
+            .padding(8.dp)
             .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(
-            text = "Hello $name!",
+        Row(
             modifier = modifier
-                .padding(16.dp)
-                .fillMaxSize(),
-            style = LabyrinthTypography.displayLarge
-        )
-        Text(
-            text = "An amazeing app",
+                .padding(4.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = LabyrinthTypography.displayLarge
+            )
+        }
+        Row(
             modifier = modifier
-                    .padding(16.dp)
-                    .fillMaxSize(),
-            style = LabyrinthTypography.bodyMedium
-        )
-
+                .padding(4.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.strap_line_text),
+                style = LabyrinthTypography.bodyMedium
+            )
+        }
+        Row(
+            modifier = modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = stringResource(R.string.overview_text),
+                style = LabyrinthTypography.bodyMedium
+            )
+        }
+        Row(
+            modifier = modifier
+                .padding(4.dp)
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(
+                modifier = modifier
+                    .padding(4.dp)
+                    .fillMaxWidth(),
+                onClick = { /*TODO*/ },
+            ) {
+                Text(
+                    text = stringResource(R.string.play_button_text),
+                    style = LabyrinthTypography.bodyMedium
+                )
+            }
+        }
     }
 }
 
@@ -66,6 +107,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LabyrinthTheme {
-        Greeting("Android")
+        Welcome()
     }
 }
